1fd763119ed31049fd6d6ea1315fceec9c6ca841
project_Html <khoanguyenduc99@gmail.com>
2024-10-01 14:05:14 +0700

    fix wave track by using "useMemo"

--- nextjs-mui-ts-starter/src/components/track/wavetrack.tsx
+++ nextjs-mui-ts-starter/src/components/track/wavetrack.tsx
@@ -1,25 +1,67 @@
-"use client";
-
-import React, { useEffect } from "react";
+import { useEffect, useRef, useState, useMemo } from "react";
 import WaveSurfer from "wavesurfer.js";
-import { useRef } from "react";
 import { useSearchParams } from "next/navigation";
 
-export default function WaveTrack() {
-  const searchParams = useSearchParams();
+// WaveSurfer hook
+const useWavesurfer = (containerRef: any, options: any) => {
+  const [wavesurfer, setWavesurfer] = useState<any>(null);
 
-  const fileName = searchParams.get("audio");
-
-  const containerRef = useRef<HTMLDivElement>(null);
+  // Initialize wavesurfer when the container mounts
+  // or any of the props change
   useEffect(() => {
-    if (containerRef.current) {
-      WaveSurfer.create({
-        container: containerRef.current,
-        waveColor: "#4F4A85",
-        progressColor: "#383351",
-        url: `/api?audio=${fileName}`,
-      });
-    }
+    if (!containerRef.current) return;
+
+    const ws = WaveSurfer.create({
+      ...options,
+      container: containerRef.current,
+    });
+
+    setWavesurfer(ws);
+
+    return () => {
+      ws.destroy();
+    };
+  }, [options, containerRef]);
+
+  return wavesurfer;
+};
+const WaveTrack = () => {
+  const searchParams = useSearchParams();
+  const fileName = searchParams.get("audio");
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const optionsMemo = useMemo(() => {
+    return {
+      waveColor: "rgb(200, 0, 200)",
+      progressColor: "rgb(100, 0, 100)",
+      url: `/api?audio=${fileName}`,
+    };
   }, []);
-  return <div ref={containerRef}>WaveTrack</div>;
-}
+
+  // const options = {
+  //     waveColor: 'rgb(200, 0, 200)',
+  //     progressColor: 'rgb(100, 0, 100)',
+  //     url: `/api?audio=${fileName}`,
+  // }
+
+  const wavesurfer = useWavesurfer(containerRef, optionsMemo);
+
+  // useEffect(() => {
+  //     if (containerRef.current) {
+  //         const wavesurfer = WaveSurfer.create({
+  //             container: containerRef.current,
+  //             waveColor: 'rgb(200, 0, 200)',
+  //             progressColor: 'rgb(100, 0, 100)',
+  //             url: `/api?audio=${fileName}`,
+  //         })
+
+  //         return () => {
+  //             wavesurfer.destroy()
+  //         }
+  //     }
+  // }, [])
+
+  return <div ref={containerRef}>wave track</div>;
+};
+
+export default WaveTrack;
