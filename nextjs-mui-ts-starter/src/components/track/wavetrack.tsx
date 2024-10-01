5ce9524bffabb475003f0f877ff03c658180af36
project_Html <khoanguyenduc99@gmail.com>
2024-10-01 14:17:56 +0700

    seperate hook

--- nextjs-mui-ts-starter/src/components/track/wavetrack.tsx
+++ nextjs-mui-ts-starter/src/components/track/wavetrack.tsx
@@ -1,30 +1,7 @@
-import { useEffect, useRef, useState, useMemo } from "react";
-import WaveSurfer from "wavesurfer.js";
+import React, { useEffect, useRef, useState, useMemo } from "react";
+import { useWavesurfer } from "@/utils/customHooks";
 import { useSearchParams } from "next/navigation";
 
-// WaveSurfer hook
-const useWavesurfer = (containerRef: any, options: any) => {
-  const [wavesurfer, setWavesurfer] = useState<any>(null);
-
-  // Initialize wavesurfer when the container mounts
-  // or any of the props change
-  useEffect(() => {
-    if (!containerRef.current) return;
-
-    const ws = WaveSurfer.create({
-      ...options,
-      container: containerRef.current,
-    });
-
-    setWavesurfer(ws);
-
-    return () => {
-      ws.destroy();
-    };
-  }, [options, containerRef]);
-
-  return wavesurfer;
-};
 const WaveTrack = () => {
   const searchParams = useSearchParams();
   const fileName = searchParams.get("audio");
@@ -38,28 +15,7 @@ const WaveTrack = () => {
     };
   }, []);
 
-  // const options = {
-  //     waveColor: 'rgb(200, 0, 200)',
-  //     progressColor: 'rgb(100, 0, 100)',
-  //     url: `/api?audio=${fileName}`,
-  // }
-
   const wavesurfer = useWavesurfer(containerRef, optionsMemo);
-
-  // useEffect(() => {
-  //     if (containerRef.current) {
-  //         const wavesurfer = WaveSurfer.create({
-  //             container: containerRef.current,
-  //             waveColor: 'rgb(200, 0, 200)',
-  //             progressColor: 'rgb(100, 0, 100)',
-  //             url: `/api?audio=${fileName}`,
-  //         })
-
-  //         return () => {
-  //             wavesurfer.destroy()
-  //         }
-  //     }
-  // }, [])
 
   return <div ref={containerRef}>wave track</div>;
 };
--- nextjs-mui-ts-starter/src/utils/customHooks.tsx
+++ nextjs-mui-ts-starter/src/utils/customHooks.tsx
@@ -1,11 +1,37 @@
 import { useState, useEffect } from "react";
+import WaveSurfer, { WaveSurferOptions } from "wavesurfer.js";
 
 export const useHasMounted = () => {
-    const [hasMounted, setHasMounted] = useState<Boolean>(false);
-    useEffect(() => {
-        setHasMounted(true);
+  const [hasMounted, setHasMounted] = useState<Boolean>(false);
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
 
-    }, []);
+  return hasMounted;
+};
+// WaveSurfer hook
+export const useWavesurfer = (
+  containerRef: React.RefObject<HTMLElement>,
+  options: Omit<WaveSurferOptions, "container">
+) => {
+  const [wavesurfer, setWavesurfer] = useState<any>(null);
 
-    return hasMounted;
-}
+  // Initialize wavesurfer when the container mounts
+  // or any of the props change
+  useEffect(() => {
+    if (!containerRef.current) return;
+
+    const ws = WaveSurfer.create({
+      ...options,
+      container: containerRef.current,
+    });
+
+    setWavesurfer(ws);
+
+    return () => {
+      ws.destroy();
+    };
+  }, [options, containerRef]);
+
+  return wavesurfer;
+};
